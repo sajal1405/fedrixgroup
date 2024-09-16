65319bf84061f58cf0aeb1a0d60873610214ac07
sajal1405 <sajal@fedrixgroup.com>
2024-09-16 04:40:08 +0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+
+    ...
+
+</head>
+
+<body>
+
+    ...
+
+    <!-- jQuery js -->
+    <script src="js/plugins/jquery.min.js"></script>
+    <!-- swup js -->
+    <script src="js/plugins/swup.min.js"></script>
+    <!-- swiper js -->
+    <script src="js/plugins/swiper.min.js"></script>
+    <!-- fancybox js -->
+    <script src="js/plugins/fancybox.min.js"></script>
+    <!-- gsap js -->
+    <script src="js/plugins/gsap.min.js"></script>
+    <!-- scroll smoother -->
+    <script src="js/plugins/smooth-scroll.js"></script>
+    <!-- scroll trigger js -->
+    <script src="js/plugins/ScrollTrigger.min.js"></script>
+    <!-- scroll to js -->
+    <script src="js/plugins/ScrollTo.min.js"></script>
+    <!-- ashley js -->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
